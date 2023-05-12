880adc9a016ce001131f1bfabf49b8ff555098ee
Angeles Cahill <angeles.cahill@gmail.com>
2023-05-12 19:59:50 -0400

    subiendo a repositorio

---
+++ assets/clientes.js
@@ -0,0 +1,12 @@
+class cliente {
+    constructor (nombre, impuesto) {
+    this.nombre = nombre;
+    this.impuesto = impuesto;
+    }
+    get nombre() {
+        return this.nombre;
+    }
+    nombre() {
+        return new nombre();
+    }
+}
